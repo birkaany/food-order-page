36a42951d17781a41b5a712b2031ead4a5373f17
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2022-12-02 15:12:46 +0300

    fix: changed data.js for img paths

--- js/data.js
+++ js/data.js
@@ -1,7 +1,7 @@
 export const pizzaMenu = [
   {
     name: "Cheese Lovers",
-    img: "/assets/img/cheese-lovers.png",
+    img: "assets/img/cheese-lovers.png",
     desc: "Combination of Alfredo sauce and two cheeses – Romano, and Parmesan.",
     price: 20,
     id: 0,
@@ -9,7 +9,7 @@ export const pizzaMenu = [
   },
   {
     name: "Pepperoni Pizza",
-    img: "/assets/img/pepperoni-pizza.png",
+    img: "assets/img/pepperoni-pizza.png",
     desc: "Pizza with homemade spicy beef sausage.",
     price: 25,
     id: 1,
@@ -17,7 +17,7 @@ export const pizzaMenu = [
   },
   {
     name: "Margherita Pizza",
-    img: "/assets/img/margherita-pizza.png",
+    img: "assets/img/margherita-pizza.png",
     desc: "Made with San Marzano tomatoes, mozzarella cheese, and fresh basil.",
     price: 35,
     id: 2,
