80ce68ddd5df1190460307f334223f197e859b22
Birkan Yılmaz <59147411+birkaany@users.noreply.github.com>
2022-12-04 04:57:17 +0300

    style: fixed mobile logo

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@ const paymentModalCloseBtn = document.getElementById("paymentCloseBtn");
 const paymentForm = document.getElementById("paymentForm");
 
 paymentForm.cardNumber.addEventListener("keydown", function (e) {
+  console.log(e.keyCode);
   if (e.keyCode !== 8) {
     if (this.value.length === 4 || this.value.length === 9 || this.value.length === 14) {
       this.value = this.value += " ";
@@ -153,7 +154,7 @@ function getMenu() {
   pizzaMenu.forEach(function (pizza) {
     menuList.innerHTML += `
     <li class="sm:w-72 grid grid-cols-[1fr_minmax(100px,_2fr)_1fr] items-center justify-center border border-[#bdbdbd] mx-4 p-5 gap-5 rounded-l-[180px] rounded-r-[30px] sm:mx-0 sm:grid-cols-1 sm:rounded-r-[0] sm:rounded-l-[0] sm:rounded-t-[180px] sm:rounded-b-[30px] sm:grid-rows-[1fr_minmax(150px,_auto)_auto]">
-          <img class="w-24 sm:w-72" src="${pizza.img}" alt="${pizza.name}" />
+          <img class="w-24 sm:w-72" src="${pizza.img}" alt="" />
           <div>
             <h3 class="font-semibold text-2xl text-left sm:text-center">${pizza.name}</h3>
             <p class="font-thin leading-[18px] mt-4 text-sm text-left sm:text-center">${pizza.desc}</p>
